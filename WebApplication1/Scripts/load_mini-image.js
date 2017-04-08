9443cdd834852d4c50bdb9bad6145ef161ba269b
mazhoranta <mazhoranta@gmail.com>
2017-04-09 02:54:55 +0300

    Project Alfa v1.02 [Only images can be uploaded]

--- WebApplication1/Scripts/load_mini-image.js
+++ WebApplication1/Scripts/load_mini-image.js
@@ -1,16 +1,24 @@
 ﻿$(document).ready(function () {
     var imageInput = $('#file');
     var imageContainer = $('#imageContainer');
+    //$('.alert-message').hide();
     $('#inputImageName').hide();
     //$('#filename').hide();
 
     function displayImage(file) {
         file = document.querySelector('input[type=file]').files[0];
+        if (!file.type.toString().includes('image')) {
+            $('input[type=file]').val("");
+            //$('.alert-message').show();
+            $('.alert-message').css('top', '100px');
+            //alert("This file type is not supported. Try uploading an image.");
+            return;
+        }
         $('#filename').val(file.name);
         var image = $('<img/>').appendTo(imageContainer);
         //$('<div/>').addClass('progress').attr('rel', '0').text('0%').appendTo(imgCont);
         //imgCont.get(0).file = file;
-        
+
         var reader = new FileReader();
         reader.onload = (function (aImg) {
             return function (e) {
@@ -19,14 +27,18 @@
             };
         })(image);
         reader.readAsDataURL(file);
+        $('#inputImageName').show();
     }
 
     imageInput.bind({
         change: function () {
             displayImage(this);
-            $('#inputImageName').show();
             //$('#filename').show();
         }
     });
+
+    $('.close-message').on('click', this, function () {
+        $('.alert-message').css('top', '-40px');
+    })
 
 })
